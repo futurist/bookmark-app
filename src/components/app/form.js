889d4f894953d623e006f21d6c874387c1ee6258
futurist <jamesyang999@gmail.com>
2018-03-26 13:02:04 +0800

    add toggleLike

---
+++ src/components/app/form.js
@@ -0,0 +1,165 @@
+import { Input, Icon, Tag, Modal } from 'antd'
+import { arrayUnique, ensureHTTP, fetchURLInfo } from '../../utils'
+import mapValue from 'map-value'
+import api from '../../api'
+import React from 'react'
+
+export default function wrapModal () {
+  return class ModalWrap extends React.Component {
+  
+    constructor(props){
+      super(...arguments)
+      const {editID, list} = this.props
+      const row = list && list.data[editID]
+      this.state=mapValue(
+        { visible: editID!=null },
+        row && {
+          id: v=> row,
+          tags: v=>({tagInput: row.tags.join(',')})
+        }
+      )
+      // console.log(this.state)
+    }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  handleOk = (e) => {
+    console.log(this.state)
+    this.setState({
+      confirmLoading: true,
+    })
+    const {state} = this
+    const method = state.id ? 'update' : 'post'
+    const url = '/bookmark/'+(state.id||'')
+    api[method](
+      url,
+      mapValue(this.state, {
+        visible: null,
+        tagInput: v => (v && { tags: v.split(/\s*,\s*/) })
+      }))
+      .then(() => {
+        this.props.close(true)
+      })
+      .catch(err => {
+        console.log('error post', err)
+      })
+  }
+
+  handleCancel = (e) => {
+    console.log(e)
+    this.props.close()
+  }
+
+  validModal = ()=>{
+    return !this.state.confirmLoading && this.state.visible
+  }
+
+  render() {
+    return <Modal
+      style={{zIndex: 9e9}}
+      title="Add new bookmark"
+      visible={this.state.visible}
+      onOk={this.handleOk}
+      confirmLoading={this.state.confirmLoading}
+      onCancel={this.handleCancel}>
+      <Input
+        ref={el => this.domUrl = el}
+        placeholder="Enter URL here"
+        prefix={
+          <Icon type="link" style={{ color: 'gray' }} />
+        }
+        suffix={
+          this.state.url && <Icon type="close-circle" onClick={
+            e => {
+              this.setState({ url: '' })
+              console.log(this.domUrl)
+              this.domUrl.focus()
+            }
+          } />
+        }
+        value={this.state.url}
+        onChange={e => {
+          const { value } = e.target
+          this.setState({ url: ensureHTTP(value) })
+        }}
+        onBlur={e => {
+          fetchURLInfo(this.state.url)
+            .then(json => {
+              console.log(json)
+              if (!json.error && this.validModal()) {
+                const {
+                  url,
+                  title,
+                  desc,
+                  favicon
+                } = json
+                this.setState(mapValue({
+                  url, title, desc, favicon
+                }, {
+                  url: v=>this.state.url ? {} : v,
+                  title: v=>this.state.title ? {} : v,
+                  desc: v=>this.state.desc ? {} : v,
+                }))
+              }
+            })
+            .catch(err => console.log(err))
+        }}
+      />
+
+      <Input
+        ref={el => this.domTitle = el}
+        style={{ marginTop: '.5rem' }}
+        placeholder="Enter Title here"
+        prefix={
+          <Icon type="info-circle-o" style={{ color: 'gray' }} />
+        }
+        suffix={
+          this.state.title && <Icon type="close-circle" onClick={
+            e => {
+              this.setState({ title: '' })
+              this.domTitle.focus()
+            }
+          } />
+        }
+        value={this.state.title}
+        onChange={e => this.setState({ title: e.target.value })}
+      />
+
+      <Input.TextArea
+        ref={el => this.domDesc = el}
+        style={{ marginTop: '.5rem' }}
+        value={this.state.desc}
+        onChange={e => this.setState({ desc: e.target.value })}
+        placeholder="Description here" autosize={{
+          minRows: 2, maxRows: 6
+        }} />
+      <Input
+        ref={el => this.domTagInput = el}
+        style={{ marginTop: '.5rem' }}
+        placeholder="Enter Tags ( ',' separated )"
+        prefix={
+          <Icon type="tags-o" style={{ color: 'gray' }} />
+        }
+        value={this.state.tagInput}
+        onChange={e => this.setState({ tagInput: e.target.value })} />
+      <div style={{ marginTop: '.5rem' }}>{
+        arrayUnique((this.state.tagInput || '')
+          .split(/\s*,\s*/))
+          .map((v, i, arr) =>
+            v && <Tag key={v} closable onClose={
+              e => {
+                arr.splice(i, 1)
+                this.setState({ tagInput: arr.join(',') })
+              }
+            }>{v}</Tag>
+          )
+      }</div>
+    </Modal>
+    }
+  }
+}
+
--- src/components/app/main.js
+++ src/components/app/main.js
@@ -1,7 +1,5 @@
 import { List, Avatar, Button, Spin } from 'antd'
-import { Input, Icon, Tag, Modal } from 'antd'
-import { arrayUnique, ensureHTTP, fetchURLInfo } from '../../utils'
-import mapValue from 'map-value'
+import { Icon } from 'antd'
 import api from '../../api'
 
 import React from 'react'
@@ -9,6 +7,7 @@ import {isEmpty} from 'cssobj-helper'
 import { connect } from 'react-redux'
 import {getList} from './main.actions'
 import {textOverflow} from '../../utils'
+import wrapModal from './form'
 
 class Main extends React.Component {
   state = {
@@ -17,7 +16,17 @@ class Main extends React.Component {
     showLoadingMore: true,
   }
 
-  getList(){
+  deleteItem = (id)=>{
+    api.delete('/bookmark/'+id).then(this.getList)
+  }
+
+  toggleLike = (index)=>{
+    const row = this.props.list.data[index]
+    row.isLike = !row.isLike
+    row && api.update('/bookmark/'+row.id, row).then(this.getList)
+  }
+
+  getList = ()=>{
     this.props.getList().then(data=>{
       this.setState({
         loadingMore: false,
@@ -68,14 +77,17 @@ class Main extends React.Component {
         itemLayout="horizontal"
         loadMore={loadMore}
         dataSource={data}
-        renderItem={item => (
-          <List.Item actions={[<a onClick={e=>this.setState({editID: item.id})}>编辑</a>, <a>删除</a>]}>
+        renderItem={(item, i) => (
+          <List.Item actions={[<a onClick={e=>this.setState({editID: i})}>编辑</a>, <a onClick={e=>this.deleteItem(item.id)}>删除</a>]}>
             <List.Item.Meta
               avatar={<Avatar size={16} src={item.favicon||''} />}
               title={<a href={item.url} title={item.url} target="_blank">{textOverflow(item.url,40)}</a>}
               description={<section>
                 <div>{item.title}</div>
-                <div>{item.tags.map(v=><a key={v}>{v} </a>)}</div>
+                <div>
+                  <Icon type={item.isLike ? 'star' : 'star-o'} style={{marginRight: '1rem'}} onClick={e=>this.toggleLike(i)} />
+                  {item.tags && item.tags.map(v=><a key={v}>{v} </a>)}
+                </div>
               </section>}
             />
             <div>{item.desc}</div>
@@ -94,7 +106,12 @@ class Main extends React.Component {
         <Icon type="plus-circle" />
       </Button>
 
-      <WrapModal editID={this.state.editID} list={list} close={()=>this.setState({editID: 0})} />
+      <WrapModal editID={this.state.editID} list={list} close={
+        (refresh)=>{
+          this.setState({editID: null})
+          if(refresh) this.getList()
+        }
+      } />
 
     </main>
   }
@@ -107,154 +124,3 @@ export default connect(
   }
 )(Main)
 
-
-
-
-function wrapModal () {
-  return class ModalWrap extends React.Component {
-  
-  state = { visible: !!this.props.editID }
-
-  showModal = () => {
-    this.setState({
-      visible: true,
-    })
-  }
-
-  handleOk = (e) => {
-    console.log(this.state)
-    this.setState({
-      confirmLoading: true,
-    })
-    api
-      .post('/bookmark', mapValue(this.state, {
-        visible: v => ({ kkk: 1234, }),
-        tagInput: v => (v && { tags: v.split(/\s*,\s*/) })
-      }))
-      .then(() => {
-        this.setState({
-          visible: false,
-        })
-        this.props.close()
-      })
-      .catch(err => {
-        console.log('error post', err)
-      })
-  }
-
-  handleCancel = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    })
-    this.props.close()
-  }
-
-  validModal = ()=>{
-    return !this.state.confirmLoading && this.state.visible
-  }
-
-  render() {
-    return <Modal
-      style={{zIndex: 9e9}}
-      title="Add new bookmark"
-      visible={this.state.visible}
-      onOk={this.handleOk}
-      confirmLoading={this.state.confirmLoading}
-      onCancel={this.handleCancel}>
-      <Input
-        ref={el => this.domUrl = el}
-        placeholder="Enter URL here"
-        prefix={
-          <Icon type="link" style={{ color: 'gray' }} />
-        }
-        suffix={
-          this.state.url && <Icon type="close-circle" onClick={
-            e => {
-              this.setState({ url: '' })
-              console.log(this.domUrl)
-              this.domUrl.focus()
-            }
-          } />
-        }
-        value={this.state.url}
-        onChange={e => {
-          const { value } = e.target
-          this.setState({ url: ensureHTTP(value) })
-        }}
-        onBlur={e => {
-          fetchURLInfo(this.state.url)
-            .then(json => {
-              console.log(json)
-              if (!json.error && this.validModal()) {
-                const {
-                  url,
-                  title,
-                  desc,
-                  favicon
-                } = json
-                this.setState(mapValue({
-                  url, title, desc, favicon
-                }, {
-                  url: v=>this.state.url ? {} : v,
-                  title: v=>this.state.title ? {} : v,
-                  desc: v=>this.state.desc ? {} : v,
-                }))
-              }
-            })
-            .catch(err => console.log(err))
-        }}
-      />
-
-      <Input
-        ref={el => this.domTitle = el}
-        style={{ marginTop: '.5rem' }}
-        placeholder="Enter Title here"
-        prefix={
-          <Icon type="info-circle-o" style={{ color: 'gray' }} />
-        }
-        suffix={
-          this.state.title && <Icon type="close-circle" onClick={
-            e => {
-              this.setState({ title: '' })
-              this.domTitle.focus()
-            }
-          } />
-        }
-        value={this.state.title}
-        onChange={e => this.setState({ title: e.target.value })}
-      />
-
-      <Input.TextArea
-        ref={el => this.domDesc = el}
-        style={{ marginTop: '.5rem' }}
-        value={this.state.desc}
-        onChange={e => this.setState({ desc: e.target.value })}
-        placeholder="Description here" autosize={{
-          minRows: 2, maxRows: 6
-        }} />
-      <Input
-        ref={el => this.domTagInput = el}
-        style={{ marginTop: '.5rem' }}
-        placeholder="Enter Tags ( ',' separated )"
-        prefix={
-          <Icon type="tags-o" style={{ color: 'gray' }} />
-        }
-        value={this.state.tagInput}
-        onChange={e => this.setState({ tagInput: e.target.value })} />
-      <div style={{ marginTop: '.5rem' }}>{
-        arrayUnique((this.state.tagInput || '')
-          .split(/\s*,\s*/))
-          .map((v, i, arr) =>
-            v && <Tag key={v} closable onClose={
-              e => {
-                arr.splice(i, 1)
-                this.setState({ tagInput: arr.join(',') })
-              }
-            }>{v}</Tag>
-          )
-      }</div>
-    </Modal>
-    }
-  }
-}
